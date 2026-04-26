742177fd4f6f636e096d68c211aa5cb87ac42385
Chris-FSC <solic@farmingdale.edu>
2026-04-26 16:46:52 -0400

    Create Constants.java

---
+++ src/main/java/org/example/Constants.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Constants {
+    public enum Size { SMALL, MEDIUM, LARGE }
+    public enum Milk { WHOLE, SKIM, ALMOND, OAT, SOY, NONE }
+    public enum Sweetener { SUGAR, HONEY, STEVIA, NONE }
+}
