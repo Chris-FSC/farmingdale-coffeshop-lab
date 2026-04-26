df280f69a5796691564ed63e446c7aab2f9b05fe
Chris-FSC <solic@farmingdale.edu>
2026-04-26 16:08:55 -0400

    Update BeverageBuilder.java

--- src/main/java/org/example/BeverageBuilder.java
+++ src/main/java/org/example/BeverageBuilder.java
@@ -2,13 +2,29 @@ package org.example;
 
 public abstract class BeverageBuilder {
    protected Beverage beverage;
-    public abstract BeverageBuilder setSize(String size);
-    public abstract BeverageBuilder setMilk(String milk);
-    public abstract BeverageBuilder setSweetener(String sweetener);
-    public abstract BeverageBuilder setShots(int shots);
-    public abstract BeverageBuilder setHot(boolean isHot);
+    public abstract BeverageBuilder setSize(String size) {
+       beverage.setSize(size);
+       return this;
+    }
+    public abstract BeverageBuilder setMilk(String milk) {
+       beverage.setMilk(milk);
+       return this;
+    }
+    public abstract BeverageBuilder setSweetener(String sweetener) {
+       beverage.setSweetener(sweetener);
+       return this;
+    }
+    public abstract BeverageBuilder setShots(int shots) {
+       beverage.setShots(shots);
+       return this;
+    }
+    public abstract BeverageBuilder setHot(boolean isHot) {
+       beverage.setHot(isHot);
+       return this;
+    }
 
     public Beverage build() {
         return beverage;
     }
+   
 }
