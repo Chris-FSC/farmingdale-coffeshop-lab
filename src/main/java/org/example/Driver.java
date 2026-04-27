5d2edd10f2047dafc71beea147f920344e318bdc
Chris <solic@farmingdale.edu>
2026-04-26 22:47:25 -0400

    Added temp (temperature) to coffee, tea and latte. And change output description. Capped shots between 0 - 3. placed iced or hot in front of drink orders

--- src/main/java/org/example/Driver.java
+++ src/main/java/org/example/Driver.java
@@ -17,16 +17,23 @@ public class Driver {
                 String type = scanner.nextLine().toLowerCase();
                 if (type.equals("ready")) break;
 
+                if (!(type.equals("coffee") || type.equals("tea") || type.equals("latte") || type.equals("cappuccino"))) {
+                    throw new IllegalArgumentException("Invalid beverage type: " + type);
+                }
+
                 System.out.print("Enter size (SMALL, MEDIUM, LARGE): ");
                 Size size = Size.valueOf(scanner.nextLine().toUpperCase());
 
                 BeverageBuilder builder = new BeverageBuilder(type, size);
 
-                System.out.print("Enter milk (WHOLE, SKIM, ALMOND, OAT, SOY, NO): ");
-                builder.updateMilk(Milk.valueOf(scanner.nextLine().toUpperCase()));
-
+                if(!type.equals("tea")) {
+                    System.out.print("Enter milk (WHOLE, SKIM, ALMOND, OAT, SOY, NO): ");
+                    builder.updateMilk(Milk.valueOf(scanner.nextLine().toUpperCase()));
+                } else {
+                    System.out.println("Skipping Milk since milk is not allowed for tea. \n");
+                }
                 if (!type.equals("tea") && !type.equals("coffee")) {
-                    System.out.print("How many shots? ");
+                    System.out.print("How many shots? (0 - 3): ");
                     int shots = Integer.parseInt(scanner.nextLine());
                     builder.updateShots(shots);
                 }
@@ -35,8 +42,14 @@ public class Driver {
                 builder.updateSweetener(Sweetener.valueOf(scanner.nextLine().toUpperCase()));
 
                 if (!type.equals("cappuccino")) {
-                    System.out.print("Would you like it hot?(If not, drink is iced) (true/false): ");
-                    builder.updateIsHot(Boolean.parseBoolean(scanner.nextLine()));
+                    System.out.print("Would you like it hot? (yes/no): ");
+                    String hotInput = scanner.nextLine().trim().toLowerCase();
+                    boolean hotBoolean = switch (hotInput) {
+                        case "yes" -> true;
+                        case "no" -> false;
+                        default -> throw new IllegalArgumentException("Must answer 'yes or 'no' for temperature.");
+                    };
+                    builder.updateIsHot(hotBoolean);
                 }
 
                 Beverage drink = builder.build();
